1cee66b6470c25f1514ee7d62e3ed12b389fa604
GakutoOkawara <ookawara0817@gmail.com>
2023-01-21 15:51:24 +0900

    fix to add tailwind class in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-import { Hello } from "../components/Hello"
-
 const RootPage = () => {
-  return <Hello />
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="py-4 px-6 text-center bg-green-600 rounded-md">
+        <h1 className="text-4xl font-bold text-white">
+          Hello Tailwind!
+        </h1>
+      </div>
+    </div>
+  )
 }
 
 export default RootPage
